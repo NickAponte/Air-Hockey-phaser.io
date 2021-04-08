299399464c7acb02a61e4f4bf4d4d232291f5245
NickAponte <apontenick@aol.com>
2021-04-07 23:13:57 -0400

    Paddle things are happening

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,8 +8,18 @@ class Game extends Phaser.Scene
         
        const ball = this.add.circle(400, 250, 10,0xffffff)
         this.physics.add.existing(ball)
+        ball.body.setBounce(1,1)
         ball.body.setCollideWorldBounds(true, 1,1)
-        ball.body.setVelocity(200, 200)
+        ball.body.setVelocity(-200,0)
+
+        const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1);
+        this.physics.add.existing(paddleLeft, true)
+        // paddleLeft.body.setBounceX(1,1)
+        // const body = paddleLeft.body 
+        // body.setMass(5000)
+        // body.setBounce(1,1)
+        this.physics.add.collider(paddleLeft, ball)
+        
     }
 }
 
